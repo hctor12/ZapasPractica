a27c9bd6f741adc1a67ed16645744bdd27e45395
hctor12 <hectorgilfuertes12@gmail.com>
2026-03-10 13:24:42 +0100

    Add project files.

---
+++ ZapasPractica/Controllers/ZapatillasController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZapasPractica.Models;
+using ZapasPractica.Repositories;
+
+namespace ZapasPractica.Controllers
+{
+    public class ZapatillasController : Controller
+    {
+        private RepositoryZapatillas repo;
+
+        public ZapatillasController(RepositoryZapatillas repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Zapatilla> zapas = await this.repo.GetZapatillasAsync();
+            return View(zapas);
+        }
+
+        public async Task<IActionResult> Details(int id, int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+
+            (int numRegistros, ImagenZapatilla imagen) = await this.repo.GetImagenZapatillaAsync(posicion.Value, id);
+            int siguiente = posicion.Value + 1;
+            if (siguiente > numRegistros)
+            {
+                siguiente = numRegistros;
+            }
+            int anterior = posicion.Value - 1;
+            if (anterior < 1)
+            {
+                anterior = 1;
+            }
+            ViewData["ULTIMO"] = numRegistros;
+            ViewData["SIGUIENTE"] = siguiente;
+            ViewData["ANTERIOR"] = anterior;
+            ViewData["POSICION"] = posicion;
+            ViewData["IMAGEN"] = imagen;
+            Zapatilla zapa = await this.repo.FindZapatillaAsync(id);
+            return View(zapa);
+        }
+
+        public async Task<IActionResult> GetImagenes(int posicion, int zapa)
+        {
+            if (zapa == null)
+            {
+                return NotFound();
+            }
+
+            (int numRegistros, ImagenZapatilla imagen) = await this.repo.GetImagenZapatillaAsync(posicion, zapa);
+            int siguiente = posicion + 1;
+            if (siguiente > numRegistros)
+            {
+                siguiente = numRegistros;
+            }
+            int anterior = posicion - 1;
+            if (anterior < 1)
+            {
+                anterior = 1;
+            }
+            ViewData["ULTIMO"] = numRegistros;
+            ViewData["SIGUIENTE"] = siguiente;
+            ViewData["ANTERIOR"] = anterior;
+            ViewData["POSICION"] = posicion;
+            return PartialView("_ImagenPartial", imagen);
+        }
+
+    }
+}
---
+++ ZapasPractica/Repositories/RepositoryZapatillas.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using ZapasPractica.Data;
+using ZapasPractica.Models;
+
+namespace ZapasPractica.Repositories
+{
+    public class RepositoryZapatillas
+    {
+        private ZapatillasContext context;
+
+        public RepositoryZapatillas(ZapatillasContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Zapatilla>> GetZapatillasAsync()
+        {
+            return await this.context.Zapatillas.ToListAsync();
+        }
+
+        public async Task<Zapatilla> FindZapatillaAsync(int id)
+        {
+            return await this.context.Zapatillas.FindAsync(id);
+        }
+
+        public async Task<(int, ImagenZapatilla)> GetImagenZapatillaAsync(int posicion, int id)
+        {
+            string sql = "SP_IMAGEN_ZAPATILLA @posicion, @idzapa, @registros out";
+            SqlParameter pamPosicion = new SqlParameter("@posicion", posicion);
+            SqlParameter pamIdzapa = new SqlParameter("@idzapa", id);
+            SqlParameter pamRegistros = new SqlParameter("@registros", 0);
+            pamRegistros.Direction = ParameterDirection.Output;
+            ImagenZapatilla imagen = await this.context.Imagenes.FromSqlRaw(sql, pamPosicion, pamIdzapa, pamRegistros).AsAsyncEnumerable().FirstOrDefaultAsync();
+            int registros = (pamRegistros.Value == DBNull.Value) ? 0 : Convert.ToInt32(pamRegistros.Value);
+            return (registros, imagen);
+        }
+    }
+}
